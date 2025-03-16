0acfaf40ee790832c611c4dfcb408b333ff80f82
Anthony O'Connell <tony@one.ie>
2025-03-16 20:55:04 +0700

    Node Adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 export default defineConfig({
   integrations: [react()],
@@ -10,10 +10,7 @@ export default defineConfig({
     plugins: [tailwindcss()],
   },
   output: 'server',
-  adapter: vercel({
-    webAnalytics: {
-      enabled: true,
-    },
-    imageService: true,
+  adapter: node({
+    mode: 'standalone'
   }),
 });
